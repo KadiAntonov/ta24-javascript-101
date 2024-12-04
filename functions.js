ca48570b1400ee34f53b59ad3eb177b8112dc918
KadiAntonov <antonov.kadi@gmail.com>
2024-12-04 16:14:58 +0200

    functions

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@
  * FUNCTIONS
  * Funktsioone kasutame korduvate tegevuste automatiseerimiseks.
  * Funktsioon võtab sulgude vahele parameetrid. (placeholders)
+ * keyword return tagastab misiganes peale teda tuleb
  */
 
 function sum(num1, num2) {
@@ -12,10 +13,20 @@ console.log(sum(4, 6))
 const sumOfTwoNumbers = sum(4, 6)
 
 /**
+ * IIFE - funktsioon, mis kutsutakse automaatselt välja.
+ */
+
+(() => {
+    console.log(2 - 3);
+})();
+
+/**
  * Arrow function 
  */
 
-function fullName(firstName, lastName) {
+const fullName = (firstName, lastName) => {
     //return firstName + " " + lastName;
     return `${firstName} ${lastName}`;
 }
+
+console.log(fullName("Kadi", "Antonov"));
